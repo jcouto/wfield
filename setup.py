d108c3e3e3bbbfe215e7c833a0c60ac0513505d8
Joao Couto <jpcouto@gmail.com>
2020-04-13 20:10:18 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Install script for widefield tools
+# Joao Couto - March 2020
+# jpcouto@gmail.com
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+longdescription = '''Utilities to look at widefield data and align with the allen reference map.'''
+setup(
+    name = 'wfieldtools',
+    version = '0.0',
+    author = 'Joao Couto',
+    author_email = 'jpcouto@gmail.com',
+    description = (longdescription),
+    long_description = longdescription,
+    license = 'GPL',
+    packages = ['wfieldtools'],
+    #entry_points = {
+    #    'console_scripts': [
+    #        'wfieldtools = wfieldtools.gui.gui:main',
+    #    ]
+    #},
+)
