ff9896e2930fffdf91ff470227aa4ffd9db91062
Joao Couto <jpcouto@gmail.com>
2020-04-15 09:31:24 -0400

    Simplified cli.

--- wfieldtools/cli.py
+++ wfieldtools/cli.py
@@ -1,5 +1,4 @@
-# These are the command line interface tools
-
+# This is the command line interface 
 import argparse
 import sys
 import os
@@ -7,8 +6,11 @@ import subprocess
 import shlex
 import platform
 from .utils import *
-
-hostname = platform.node()
+from .io import parse_imager_mj2_folder,mmap_dat
+from .io import frames_average_for_trials
+from .registration import motion_correct
+from .decomposition import approximate_svd
+from .hemocorrection import hemodynamic_correction
 
 class CLIParser(object):
     def __init__(self):
@@ -19,11 +21,11 @@ class CLIParser(object):
 The commands are:
     open                Opens a gui to look at the preprocessed data        
     imager              Preprocesses data recorded with the WidefieldImager
-    register            Registers data from a binary file
+    motion              Registers data from a binary file
     decompose           Performs single value decomposition
     correct             Performs hemodynamic correction on dual channel data
 ''')
-        parser.add_argument('command', help='Subcommand to run')
+        parser.add_argument('command', help='type wfieldtools <command> -h for help')
         # parse_args defaults to [1:] for args, but you need to
         # exclude the rest of the args too, or validation will fail
         args = parser.parse_args(sys.argv[1:2])
@@ -58,14 +60,23 @@ The commands are:
 
     def imager(self):
         parser = argparse.ArgumentParser(
-            description='Open the GUI to look at preprocessed data')
-        parser.add_argument('foldername', action='store',default=None,type=str)
-        parser.add_argument('-o','--output', action='store',default=None,type=str) # there should be an intermediate folder as well
-        parser.add_argument('-k', action='store',default=200,type=int) # number of components
-        parser.add_argument('--mask-edge', action='store',default=30,type=int) # mask edge for motion correction
-        parser.add_argument('--nbaseline-frames', action='store',default=30,type=int) # number of baseline frames
-        parser.add_argument('--fs', action='store',default=30.,type=np.float32) # sampling rate of a single channel
-
+            description='Performs preprocessing of widefield data recorded with the WidefieldImager')
+        parser.add_argument('foldername', action='store',
+                            default=None, type=str,
+                            help='Folder where to search for mj2 and analog files.')
+        parser.add_argument('-o','--output', action='store',
+                            default=None, type=str,
+                            help='Output folder') # there should be an intermediate folder as well
+        parser.add_argument('-k', action='store',default=200,type=int,
+                            help = 'Number of components for SVD')
+        parser.add_argument('--mask-edge', action='store',default=30,type=int,
+                            help = 'Size of the mask used on the edges during motion correction ') 
+        parser.add_argument('--nbaseline-frames', action='store',
+                            default=30, type=int,
+                            help='Number of frames to compute the  baseline')
+        parser.add_argument('--fs', action='store',default=30.,type=np.float32,
+                            help='Sampling frequency of an individual channel')
+        
         args = parser.parse_args(sys.argv[2:])
         remotepath = args.foldername
         localdisk = args.output # this should be an SSD or a fast drive
@@ -85,7 +96,6 @@ The commands are:
         {1}
 
         '''.format(remotepath,localdisk))
-        from .io import parse_imager_mj2_folder,mmap_dat
         tfetch = time.time()
         dat,frames_avg, trialonsets,trialinfo = parse_imager_mj2_folder(remotepath, localdisk)
         del dat
@@ -95,53 +105,102 @@ The commands are:
         tfetch = (time.time() - tfetch)/60.
 
         tproc = time.time()
-
         # MOTION CORRECTION
-        dat_path = glob(pjoin(localdisk,'*.dat'))[0]        
-        dat = mmap_dat(dat_path, mode='r+')
-        from .registration import motion_correct
-        yshifts,xshifts,avg_dat = motion_correct(dat,chunksize=512,
-                                                 mask_edge=args.mask_edge,
-                                                 apply_shifts=True)
-        del dat # close and finish writing
-        shifts = np.rec.array([yshifts,xshifts],dtype=[('y','int'),('x','int')])
-        np.save(pjoin(localdisk,'motion_correction_shifts.npy'),shifts)
-        np.save(pjoin(localdisk,'frames_average.npy'),avg_dat)
-        del shifts
-        
-        dat_path = glob(pjoin(localdisk,'*.dat'))[0]
+        _motion(localdisk,args.mask_edge)
         # COMPUTE AVERAGE FOR BASELINE
-        dat = mmap_dat(dat_path)
-        trial_onsets = np.load(pjoin(localdisk,'trial_onsets.npy'))
-        from .io import frames_average_for_trials
-        frames_average = frames_average_for_trials(dat,
-                                                   trial_onsets['iframe'],
-                                                   args.nbaseline_frames)
-        np.save(pjoin(localdisk,'frames_average.npy'),frames_average)
-        del dat
-        del frames_average
+        _baseline(localdisk,args.nbaseline_frames)
         # DATA REDUCTION
-        dat_path = glob(pjoin(localdisk,'*.dat'))[0]
-        frames_average = np.load(pjoin(localdisk,'frames_average.npy'))
-        trial_onsets = np.load(pjoin(localdisk,'trial_onsets.npy'))
-        
-        dat = mmap_dat(dat_path) # load to memory if you have enough
-        onsets = trial_onsets['iframe']
-        from .decomposition import approximate_svd
-        U,SVT = approximate_svd(dat, frames_average,onsets = onsets)
-        np.save(pjoin(localdisk,'Ua.npy'),U)
-        np.save(pjoin(localdisk,'SVTa.npy'),SVT)
-        from .hemocorrection import hemodynamic_correction
-        SVTcorr, rcoeffs, T = hemodynamic_correction(U, SVT, fs=args.fs)        
-
-        np.save(pjoin(localdisk,'rcoeffs.npy'),rcoeffs)
-        np.save(pjoin(localdisk,'T.npy'),T)
-        np.save(pjoin(localdisk,'SVTcorr.npy'),SVTcorr)
-        del dat
+        _decompose(localdisk,k=args.k)
+        # HEMODYNAMIC CORRECTION
+        _hemocorrect(localdisk,fs=args.fs)
         tproc = (time.time() - tproc)/60.
         print('Done fetching data ({0} min) and pre-processing ({1} min)'.format(tfetch,tproc))
         exit(0)
 
+    def motion(self):
+        parser = argparse.ArgumentParser(
+            description='Performs motion correction on widefield data')
+        parser.add_argument('foldername', action='store',
+                            default=None, type=str,
+                            help='Folder with dat file.')
+        parser.add_argument('--mask-edge', action='store',default=30,type=int,
+                            help = 'Size of the mask used on the edges during motion correction ') 
+        
+        args = parser.parse_args(sys.argv[2:])
+        localdisk = args.foldername 
+        _motion(localdisk,args.mask_edge)
+    def decompose(self):
+        parser = argparse.ArgumentParser(
+            description='Performs single value decomposition')
+        parser.add_argument('foldername', action='store',
+                            default=None, type=str,
+                            help='Folder with dat file.')
+        parser.add_argument('-k', action='store',default=200,type=int,
+                            help = 'Number of components for SVD ') 
+        
+        args = parser.parse_args(sys.argv[2:])
+        localdisk = args.foldername 
+        _decompose(localdisk,k=args.k)
+    def correct(self):
+        parser = argparse.ArgumentParser(
+            description='Performs hemodynamics correction')
+        parser.add_argument('foldername', action='store',
+                            default=None, type=str,
+                            help='Folder with U and SVT files.')
+        parser.add_argument('--fs', action='store',default=30.,type=np.float32,
+                            help = 'Sampling rate of an individual channel ') 
+        
+        args = parser.parse_args(sys.argv[2:])
+        localdisk = args.foldername 
+
+        _hemocorrect(localdisk,fs=args.fs)
+        
+def _motion(localdisk,mask_edge):
+    dat_path = glob(pjoin(localdisk,'*.dat'))[0]        
+    dat = mmap_dat(dat_path, mode='r+')
+    yshifts,xshifts,avg_dat = motion_correct(dat,chunksize=512,
+                                             mask_edge=mask_edge,
+                                             apply_shifts=True)
+    del dat # close and finish writing
+    shifts = np.rec.array([yshifts,xshifts],dtype=[('y','int'),('x','int')])
+    np.save(pjoin(localdisk,'motion_correction_shifts.npy'),shifts)
+    np.save(pjoin(localdisk,'frames_average.npy'),avg_dat)
+    del shifts
+
+def _baseline(localdisk,nbaseline_frames):
+    dat_path = glob(pjoin(localdisk,'*.dat'))[0]
+    dat = mmap_dat(dat_path)
+    trial_onsets = np.load(pjoin(localdisk,'trial_onsets.npy'))
+    
+    frames_average = frames_average_for_trials(dat,
+                                               trial_onsets['iframe'],
+                                               nbaseline_frames)
+    np.save(pjoin(localdisk,'frames_average.npy'),frames_average)
+    del dat
+    del frames_average
+
+def _decompose(localdisk, k):
+    dat_path = glob(pjoin(localdisk,'*.dat'))[0]
+    frames_average = np.load(pjoin(localdisk,'frames_average.npy'))
+    trial_onsets = np.load(pjoin(localdisk,'trial_onsets.npy'))
+        
+    dat = mmap_dat(dat_path) # load to memory if you have enough
+    onsets = trial_onsets['iframe']
+    U,SVT = approximate_svd(dat, frames_average,onsets = onsets,k = k)
+    np.save(pjoin(localdisk,'Ua.npy'),U)
+    np.save(pjoin(localdisk,'SVTa.npy'),SVT)
+
+def _hemocorrect(localdisk,fs):
+    U = np.load(pjoin(localdisk,'Ua.npy'))
+    SVT = np.load(pjoin(localdisk,'SVTa.npy'))
+
+    SVTcorr, rcoeffs, T = hemodynamic_correction(U, SVT, fs=fs)        
+
+    np.save(pjoin(localdisk,'rcoeffs.npy'),rcoeffs)
+    np.save(pjoin(localdisk,'T.npy'),T)
+    np.save(pjoin(localdisk,'SVTcorr.npy'),SVTcorr)
+            
+    
 def main():
     CLIParser()
 
