57c409ccb6325df378103895ed9b6923c0af68c0
Joao Couto <jpcouto@gmail.com>
2020-04-16 20:33:08 -0400

    Pixel correlation added to SVD viewer.

--- wfieldtools/widgets.py
+++ wfieldtools/widgets.py
@@ -25,12 +25,36 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import Qt,QTimer
 
 import pyqtgraph as pg
-pg.setConfigOption('background', [0,0,0])
-pg.setConfigOption('crashWarning', True)
+pg.setConfigOption('crashWarning', False)
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
 pg.setConfigOptions(imageAxisOrder='row-major')
+axiscolor = 'k'
+class ImageWidget(QWidget):
+    def __init__(self):
+        super(ImageWidget,self).__init__()
 
+        self.layout = QFormLayout()
+        self.setLayout(self.layout)
+        self.win = pg.GraphicsLayoutWidget()
+        self.pl = self.win.addPlot()
+        self.pl.getViewBox().invertY(True)
+        self.pl.getViewBox().setAspectLocked(True)
+        
+        self.im = pg.ImageItem()
+        self.win.setCentralWidget(self.pl)
+        self.pl.addItem(self.im)
+        
+        self.pl.getAxis('bottom').setPen(axiscolor)
+        self.pl.getAxis('left').setPen(axiscolor)
+        self.layout.addRow(self.win)
 
-class DisplayWidget(QWidget):
+    def _add_hist(self,levelMode='mono'):
+        self.hist = pg.HistogramLUTItem(levelMode = levelMode)
+        self.hist.setImageItem(self.im)
+        self.pl.addItem(self.hist)
+
+class DisplayWidget(ImageWidget):
     def __init__(self, stack,parent=None):
         super(DisplayWidget,self).__init__()
         self.parent = parent
@@ -38,57 +62,13 @@ class DisplayWidget(QWidget):
         self.iframe = 0
         tmp = self.stack[:np.clip(100,0,len(self.stack))]
         self.levels = np.percentile(tmp,[5,99])
-        self.init_ui()
-        self.set_image(0)
-
-    def init_ui(self):
-        layout = QFormLayout()
-        self.setLayout(layout)
-        win = pg.GraphicsLayoutWidget()
-        self.pl = win.addPlot()
-        self.pl.getViewBox().invertY(True)
-        self.pl.getViewBox().setAspectLocked(True)
-        
-        self.im = pg.ImageItem()
-        #from matplotlib import cm
-        # Get the colormap
-        #colormap = cm.get_cmap("gray")#"nipy_spectral")  # cm.get_cmap("CMRmap")
-        #colormap._init()
-        #lut = (colormap._lut * 255).view(np.ndarray)  # Convert matplotlib colormap from 0-1 to 0 -255 for Qt
-        # Apply the colormap
-        #self.im.setLookupTable(lut)
-        win.setCentralWidget(self.pl)
-        self.im.setImage(self.stack[0])
-        self.pl.addItem(self.im)
-        self.hist = pg.HistogramLUTItem()
-        self.hist.setImageItem(self.im)
+        self._init_ui()
+        self._add_hist()
         self.hist.setHistogramRange(*self.levels)
-        self.pl.addItem(self.hist)
-                
-        self.pl.getAxis('bottom').setPen('w')
-        self.pl.getAxis('left').setPen('w')
-        
-        def mouseMoved(pos):
-            modifiers = QApplication.keyboardModifiers()
-            pos = self.im.mapFromScene(pos.scenePos())
-            #pos = pos.pos()
-            if bool(modifiers == Qt.ControlModifier):
-                #self.roi[0].plots[0].setData(self.x, self.get_xy(pos.x(),pos.y()), 
-                #                 pen = "w", clear = True)
-                #self.parent.roiwidget.add_roi(pos)
-            #elif
-                self.parent.roiwidget.add_roi((pos.x(),pos.y()))
+        self.set_image(0)
+        self.win.scene().sigMouseClicked.connect(self.mouseMoved)    
 
-
-        #win.scene().sigMouseMoved.connect(mouseMoved)    
-        win.scene().sigMouseClicked.connect(mouseMoved)    
-
-        #self.x = np.arange(len(self.stack))
-        #win2 = pg.GraphicsLayoutWidget()
-        #self.plt = win2.addPlot()
-        #self.roi = self.plt.plot(x = np.arange(len(self.stack)),
-        #                         y = self.get_xy(250,250),color='w')
-        #self.plt.addItem(self.roi)
+    def _init_ui(self):
         widget = QWidget()
         slayout = QFormLayout()
         widget.setLayout(slayout)
@@ -100,20 +80,20 @@ class DisplayWidget(QWidget):
     
         self.wframelabel = QLabel('Frame {0:d}:'.format(self.wframe.value()))
         slayout.addRow(self.wframelabel, self.wframe)
-        layout.addRow(win)
-        layout.addRow(widget)
+        self.layout.addRow(widget)
         def uframe(val):
             i = self.wframe.value()
             self.wframelabel.setText('Frame {0:d}:'.format(i))
             self.set_image(i)
             self.parent.roiwidget.p1.setRange(xRange=self.parent.roiwidget.xlim+i)
         self.wframe.valueChanged.connect(uframe)
-        
+       
     def set_image(self,i=None):
         if not i is None:
             self.iframe = i
         img = self.stack[self.iframe]
         self.im.setImage(img,levels = self.levels)
+        
     def get_xy(self,x,y):
         x = int(np.clip(x,0,self.stack.shape[1]))
         y = int(np.clip(y,0,self.stack.shape[2]))
@@ -121,15 +101,27 @@ class DisplayWidget(QWidget):
         t = np.dot(self.stack.U[idx,:],self.stack.SVT)
         return t
 
+    def mouseMoved(self,pos):
+        modifiers = QApplication.keyboardModifiers()
+        pos = self.im.mapFromScene(pos.scenePos())
+        #pos = pos.pos()
+        if bool(modifiers == Qt.ControlModifier):
+            self.parent.roiwidget.add_roi((pos.x(),pos.y()))
+
+
 class SVDViewer(QMainWindow):
     def __init__(self,stack):
         super(SVDViewer,self).__init__()
+        self.setWindowTitle('wfield') 
         self.stack = stack
         self.setDockOptions(QMainWindow.AllowTabbedDocks |
                             QMainWindow.AllowNestedDocks |
                             QMainWindow.AnimatedDocks)
+
         self.displaywidget = DisplayWidget( self.stack,parent = self)
         self.roiwidget = ROIPlotWidget(stack,self.displaywidget.pl,self.displaywidget.im)
+        self.localcorrwidget = LocalCorrelationWidget(stack)
+
         self.svdtab = QDockWidget('Reconstructed')
         self.svdtab.setWidget(self.displaywidget)
         self.addDockWidget(Qt.RightDockWidgetArea,self.svdtab)
@@ -137,6 +129,10 @@ class SVDViewer(QMainWindow):
         self.roitab.setWidget(self.roiwidget)
         self.addDockWidget(Qt.BottomDockWidgetArea,self.roitab)
         self.set_dock(self.roitab,False)
+        self.lcorrtab = QDockWidget('Pixel correlation')
+        self.lcorrtab.setWidget(self.localcorrwidget)
+        self.addDockWidget(Qt.RightDockWidgetArea,self.lcorrtab)
+        self.tabifyDockWidget(self.lcorrtab,self.svdtab)
         self.show()
 
     def set_dock(self,dock,floating=False):
@@ -148,6 +144,42 @@ class SVDViewer(QMainWindow):
                          QDockWidget.DockWidgetFloatable)
         dock.setFloating(floating)
 
+class LocalCorrelationWidget(ImageWidget):
+    def __init__(self, stack,parent=None):
+        super(LocalCorrelationWidget,self).__init__()
+        self.parent = parent
+        self.stack = stack
+        U = stack.U
+        SVT = stack.SVT
+        from .utils_svd import svd_pix_correlation
+        self.localcorr = svd_pix_correlation(U,SVT,
+                                             dims = self.stack.shape[1:],
+                                             norm_svt=True)
+        self.levels = [0,1.]
+
+        from matplotlib.pyplot import get_cmap
+
+        pos = np.array([0, 0.5, 1.])
+        color = np.array([[0,0,255,255], [255,255,255,255], [255,0,0,255]], dtype=np.ubyte)
+        cmap = pg.ColorMap(pos, color)
+        lut = cmap.getLookupTable(0, 1.0, 256)
+
+        self._add_hist()
+        self.hist.gradient.setColorMap(cmap)
+        self.hist.setHistogramRange(*self.levels)
+
+        self.set_image([200,200])
+        self.win.scene().sigMouseMoved.connect(self.mouseMoved)
+        
+    def set_image(self,xy=[0,0]):
+        img = (self.localcorr.get(*xy)+1)/2.
+        self.im.setImage(img)
+
+    def mouseMoved(self,pos):
+        modifiers = QApplication.keyboardModifiers()
+        pos = self.im.mapFromScene(pos)
+        if bool(modifiers == Qt.ControlModifier):
+            self.set_image((int(pos.y()),int(pos.x())))
 
 class ROIPlotWidget(QWidget):
     colors = ['#d62728',
@@ -169,8 +201,8 @@ class ROIPlotWidget(QWidget):
         self.roi_target = roi_target
         win = pg.GraphicsLayoutWidget(parent=self)
         self.p1 = win.addPlot()
-        self.p1.getAxis('bottom').setPen('k') 
-        self.p1.getAxis('left').setPen('k') 
+        self.p1.getAxis('bottom').setPen(axiscolor) 
+        self.p1.getAxis('left').setPen(axiscolor) 
         layout.addWidget(win,0,0)
         self.xlim = np.array([0,1000])
         self.N = npoints
@@ -206,3 +238,35 @@ class ROIPlotWidget(QWidget):
                            self.stack.SVT.astype('float32')),axis=0).astype('float32')
         self.plots[i].setData(x = self.time,
                               y = t+self.offset*i)
+
+
+
+        #from matplotlib import cm
+        # Get the colormap
+                
+        
+
+            #self.roi[0].plots[0].setData(self.x, self.get_xy(pos.x(),pos.y()), 
+            #                 pen = "w", clear = True)
+            #self.parent.roiwidget.add_roi(pos)
+            #elif
+
+
+        #win.scene().sigMouseMoved.connect(mouseMoved)    
+
+        #self.x = np.arange(len(self.stack))
+        #win2 = pg.GraphicsLayoutWidget()
+        #self.plt = win2.addPlot()
+        #self.roi = self.plt.plot(x = np.arange(len(self.stack)),
+        #                         y = self.get_xy(250,250),color='w')
+        #self.plt.addItem(self.roi)
+        '''
+        try:
+            cmap_plt = get_cmap(cmap)
+        except:
+            cmap_plt = get_cmap('jet')
+        steps = np.linspace(0, 255, 256)
+        clrmap_pg = pg.ColorMap(steps, cmap_plt(steps))
+        self.im.setLookupTable(clrmap_pg.getLookupTable())
+        '''
+
