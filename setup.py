b2609161a9802fab2b00f187895ee257412c8c3f
Joao Couto <jpcouto@gmail.com>
2023-07-19 16:23:06 -0700

    Pumped version to release.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("requirements.txt","r") as f:
     
 setup(
     name = 'wfield',
-    version = '0.4.0',
+    version = '0.4.1',
     author = 'Joao Couto',
     author_email = 'jpcouto@gmail.com',
     description = (description),
